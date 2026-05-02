7d4e85af22c52b70ade9231d289304043fc572b4
ByGravityyy <jamesgardiner99@gmail.com>
2026-05-02 17:47:49 +0100

    test

--- Horror-Game/Assets/Scripts/MultiplayerPlayerController.cs
+++ Horror-Game/Assets/Scripts/MultiplayerPlayerController.cs
@@ -1,18 +1,81 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class MultiplayerPlayerController : MonoBehaviour
+[RequireComponent(typeof(CharacterController))]
+public class MultiplayerPlayerController : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+    public float mouseSensitivity = 2f;
+    public float gravity = -20f;
+
+    [Header("References")]
+    public Camera playerCamera;
+
+    private CharacterController controller;
+    private float verticalVelocity;
+    private float cameraPitch;
+
+    private void Awake()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnNetworkSpawn()
     {
-        
+        if (!IsOwner)
+        {
+            if (playerCamera != null)
+                playerCamera.enabled = false;
+
+            AudioListener listener = GetComponentInChildren<AudioListener>();
+            if (listener != null)
+                listener.enabled = false;
+
+            enabled = false;
+            return;
+        }
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        if (playerCamera != null)
+            playerCamera.enabled = true;
+    }
+
+    private void Update()
+    {
+        HandleLook();
+        HandleMovement();
+    }
+
+    private void HandleLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        transform.Rotate(Vector3.up * mouseX);
+
+        cameraPitch -= mouseY;
+        cameraPitch = Mathf.Clamp(cameraPitch, -80f, 80f);
+
+        playerCamera.transform.localRotation = Quaternion.Euler(cameraPitch, 0f, 0f);
+    }
+
+    private void HandleMovement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        if (controller.isGrounded && verticalVelocity < 0)
+            verticalVelocity = -2f;
+
+        verticalVelocity += gravity * Time.deltaTime;
+
+        move.y = verticalVelocity;
+
+        controller.Move(move * moveSpeed * Time.deltaTime);
     }
 }
